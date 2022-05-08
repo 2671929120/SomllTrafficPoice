a4d11b3179dd9389cdd6b862722d08a69eb1de3f
pan <2671929120@qq.com>
2022-05-09 00:49:36 +0800

    修改

--- Assets/Scripts/TrafficPolice/TrafficPolice.cs
+++ Assets/Scripts/TrafficPolice/TrafficPolice.cs
@@ -47,6 +47,7 @@ public class TrafficPolice : MonoBehaviour
             CarManager.Instance.EastMiddleStopEventStart();
         }
         //这里面还需要添加对应的动画信息
+        //
     } 
     public void EastLeftMove()
     {
